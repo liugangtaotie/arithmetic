8dff4c7ca5b834d56b92385b20bf6b84341349c0
liugangtaotie <1532183639@qq.com>
2019-05-21 10:49:59 +0800

    增加算法三正则

--- src/App.js
+++ src/App.js
@@ -1,147 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {RangeNumberNoRepeatSort,BinarySearch,JsAsyncEvent} from './arithmetic/common.js'
+import {RangeNumberNoRepeatSort,BinarySearch,JsAsyncEvent,JsRegexCommon} from './arithmetic/common.js'
 
 function App() {
   console.info(1111,RangeNumberNoRepeatSort());
-  // console.info('JsAsyncEvent',JsAsyncEvent());
+  // console.info('JsRegexCommon',JsRegexCommon());
   // console.info(2222,BinarySearch([1,2,3,4,5,6],3,0,5));
   // console.info(2222,BinarySearch([1,2,3,4,5,6],3,0,5));
+  // 浏览器渲染
 
-  setTimeout(()=>{
-    console.log('timer1')
-
-    Promise.resolve().then(function() {
-        console.log('promise1')
-    })
-}, 0)
-
-setTimeout(()=>{
-    console.log('timer2')
-
-    Promise.resolve().then(function() {
-        console.log('promise2')
-    })
-}, 0)
-
-//   setTimeout(function(){
-//     console.log('1')
-// });
-
-// new Promise(function(resolve){
-//     console.log('2');
-//                 resolve();
-// }).then(function(){
-//     console.log('3')
-// });
-
-// console.log('4');
-
-// function Person() {
-//   this.test = function() {
-//       alert("Person");
-//   }
-// }
-
-// function Dog() {
-//   this.test = function() {
-//       alert("Dog");
-//   }
-// }
-
-// var v = new Person();
-// alert(v.constructor);
-
-// v = new Dog();
-// alert(v.constructor);
+  // 来至于谷友的一到面试题
+async function async1() {
+  console.log('async1 start');
+  await async2();
+  console.log('async1 end');
+}
+async function async2() {
+  console.log('async2');
+}
+console.log('script start');
+setTimeout(function() {
+    console.log('setTimeout');
+}, 0);
+async1();
+new Promise(function(resolve) {
+    console.log('promise1');
+    resolve();
+  }).then(function() {
+    console.log('promise2');
+  });
+console.log('script end');
 
 
+//   setTimeout(_ => console.log(4))
 
-
-// console.log('1');
-
-// setTimeout(()=>{
-//   console.info('2')
-// },0)
-
-// setImmediate(()=>{
-//   console.info('3');
-// })
-
-// new Promise(function(resolve) {
-//   console.log('4');
-//   resolve();
-//   }).then(function() {
-//   console.log('5')
-//   })
-
-//   new Promise(function(resolve) {
-//     console.log('6');
-//     resolve();
-//     }).then(function() {
-//     console.log('7')
+// new Promise(resolve => {
+//   resolve()
+//   console.log(1)
+// }).then(_ => {
+//   console.log(3)
+//   Promise.resolve().then(_ => {
+//     console.log('before timeout')
+//   }).then(_ => {
+//     Promise.resolve().then(_ => {
+//       console.log('also before timeout')
 //     })
- 
-
-
-// process.nextTick(function() {
-//   console.log('8');})
-
- 
-
-// process.nextTick(function() {
-//   console.log('9');})
-
-//   process.nextTick(()=>{
-//     console.info('10')
-//   }
-//     )
-
-// console.info('11');
-
-// new Promise(function(resolve) {
-// console.log('7');
-// resolve();
-// }).then(function() {
-// console.log('8')
-// })
-
-// //加入两个nextTick的回调函数
-// process.nextTick(function () {
-//   console.log('1');
-// });
-// process.nextTick(function () { 
-//   console.log('2');
-// });
-// // 加入两个setImmediate()的回调函数
-// setImmediate(function () {
-//   console.log('3'); 
-//   // 进入下次循环 
-//   process.nextTick(function () {
-//       console.log('4');
-//   });
-// });
-// setImmediate(function () {
-//   console.log('5'); 
-// });
-
-// console.log('6');
-
-
-// setImmediate((/* 回调1 */) => {
-//   console.log('setImmediate1')
-//   setImmediate(() => {
-//       console.log('setImmediate2')
-//   })
-//   process.nextTick(() => {
-//       console.log('nextTick')
 //   })
 // })
-// setImmediate((/* 回调2 */) => {
-//   console.log('setImmediate3')
-// })
 
+// console.log(2)
 
 
   return (
--- src/arithmetic/common.js
+++ src/arithmetic/common.js
@@ -23,9 +23,8 @@ export function BinarySearch(arr, str, leftIndex, rightIndex) {
 
 // 算法三   理解js的任务队列（宏任务、微任务）
 export function JsAsyncEvent() {
-    console.log('1');
-
-    setTimeout(function () {
+    console.log('1'); // 主程序首先执行
+    setTimeout(function () { // 将事件插入了"任务队列"，必须等到当前代码（执行栈）执行完，主线程才会去执行它指定的回调函数
         console.log('2');
         process.nextTick(function () {
             console.log('3');
@@ -36,37 +35,33 @@ export function JsAsyncEvent() {
         }).then(function () {
             console.log('5')
         })
-    })
-
+    }, 1000)
     new Promise(function (resolve) {
         console.log('6');
         resolve();
     }).then(function () {
         console.log('7')
     })
-
+    setTimeout(function () { // 将事件插入了"任务队列"，必须等到当前代码（执行栈）执行完，主线程才会去执行它指定的回调函数
+        console.log('18');
+    })
     process.nextTick(function () {
-        console.log('8');
+        console.log('8'); // 在当前"执行栈"的尾部-->下一次Event Loop（主线程读取"任务队列"）之前-->触发process指定的回调函数
     })
-
     setImmediate(() => {
-        console.info('9')
+        console.info('9') // 主线程和事件队伍的函数执行完成之后立即执行  和setTimeOut(fn,0)差不多
     })
-
     new Promise(function (resolve) {
         console.log('10');
         resolve();
     }).then(function () {
         console.log('11')
     })
-
     setTimeout(function () {
         console.log('12');
-
         setImmediate(() => {
             console.info('13')
         })
-
         process.nextTick(function () {
             console.log('14');
         })
@@ -77,16 +72,31 @@ export function JsAsyncEvent() {
             console.log('16')
         })
     })
-
     process.nextTick(function () {
         console.log('17');
     })
 }
 
+// 算法四   理解正则表达式(. * + ?)
+export function JsRegexCommon() {
+    // 正则手机号码
+    const phoneStr = '18658126322';
+    const regPhone = /^1[3/4/5/6/7/8/9][0-9]{9}$/;
+    console.info('1111', regPhone.test(phoneStr));
 
+    // 正则邮箱
+    const regEmail = /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/;
+
+    const regTest = /^[a-z0-9]/;
+    console.info(2222, regTest.test('#'));
+
+
+
+}
 
 export default {
     RangeNumberNoRepeatSort,
     BinarySearch,
     JsAsyncEvent,
+    JsRegexCommon,
 }
